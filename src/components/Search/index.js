e07ae827ff1a59092d523243725db603b1279265
Ankit Kumar <itankzdev@gmail.com>
2022-01-06 23:04:42 +0530

    feat: update search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,12 @@
+import searchIcon from 'static/icons/search.svg';
 import './style.scss';
 
 const Search = () => {
     return (
-        <div>
-            Search
+        <div className='search-container'>
+            <span className='search-label'>Search</span>
+            <input type='text' placeholder='Enter here to search' className='search-input' />
+            <img className='search-icon' src={searchIcon} alt='search icon' onClick={() => {console.log("search")}}/>
         </div>
     );
 };
