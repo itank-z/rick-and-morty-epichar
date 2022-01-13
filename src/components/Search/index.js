ee0f495fa7b7b19ff5a4fa15210413f17e421b82
Ankit Kumar <itankzdev@gmail.com>
2022-01-13 23:18:57 +0530

    feat: update character component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import searchIcon from 'static/icons/search.svg';
 import './style.scss';
 
-const Search = () => {
+const Search = ({ handleSearch }) => {
+    const [searchQuery, setSearchQuery] = useState('');
+
     return (
         <div className='search-container'>
             <span className='search-label'>Search</span>
-            <input type='text' placeholder='Enter here to search' className='search-input' />
-            <img className='search-icon' src={searchIcon} alt='search icon' onClick={() => {console.log("search")}}/>
+            <input type='text' placeholder='Enter here to search' className='search-input'
+                value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }}
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
+                        handleSearch(searchQuery);
+                    }
+                }}
+            />
+            <img className='search-icon' src={searchIcon} alt='search icon' onClick={() => { handleSearch(searchQuery) }} />
         </div>
     );
 };
